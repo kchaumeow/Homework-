cfad8197cf4a061dcc10aa092e80a45ce483acaa
kchaumeow <71407757+kchaumeow@users.noreply.github.com>
2021-10-08 01:14:11 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -341,3 +341,32 @@ int main() {
 	}
 	return 3;
 }
+
+// SINUSOIDA
+#include <cmath>
+#include <Windows.h>
+int main()
+    {
+        HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+        GetConsoleScreenBufferInfo(handle, &consoleInfo);
+        int height = consoleInfo.srWindow.Bottom - consoleInfo.srWindow.Top + 1;
+        int width = consoleInfo.srWindow.Right - consoleInfo.srWindow.Left + 1;
+
+        auto GetX = [&](double x) { return int(x / 7 * width); };
+        auto GetY = [&](double y) { return int((-y / 1 + 1) * (height / 2)); };
+
+        _COORD c;
+        for (double i = 0; i < 7; i += 0.02)
+        {
+            c.X = GetX(i);
+            c.Y = GetY(sin(i));
+            SetConsoleCursorPosition(handle, c);
+            cout << '*';
+        }
+
+        cin.get();
+        CloseHandle(handle);
+
+        return 0;
+    }
